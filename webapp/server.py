7646a028e9c5415e375907a83eceaf50f083d0c3
Tjan Ljubesek <tljubesek@gmail.com>
2018-11-24 14:00:13 +0100

    server method update

--- webapp/server.py
+++ webapp/server.py
@@ -1,32 +1,58 @@
 import cherrypy
+import glob
+import os
 
-class AutoKroky(object):
+
+class AutoKrokyServer(object):
+	def __init__(self):
+		# Preload html files
+		self.html_files = {}
+		for file in glob.glob("../html/*.html"):
+			self.html_files[os.path.basename(file)] = open(file, encoding="utf8").read()
+
+	def check_login(self):
+		if not cherrypy.session.get("username"):
+			raise cherrypy.HTTPRedirect("/login")
 
 	@cherrypy.expose
 	def index(self):
-		if not cherrypy.session.get("username"):
-			raise cherrypy.HTTPRedirect("/profile")
-		return open("index.html", encoding="utf8")
+		self.check_login()
+		return self.html_files["index.html"]
 
 	@cherrypy.expose
 	def profile(self):
-		cherrypy.session['username'] = "dasd"
-		return open("profile.html", encoding="utf8")
+		self.check_login()
+		return self.html_files["profile.html"]
 
 	@cherrypy.expose
 	def preferences(self):
-		return open("preferences.html")
+		self.check_login()
+		return self.html_files["preferences.html"]
+
+	@cherrypy.expose
+	def login(self):
+		cherrypy.session["username"] = "Tjan"
+		return self.html_files["login.html"]
 
 	@cherrypy.expose
 	def logout(self):
 		cherrypy.lib.sessions.expire()
+		raise cherrypy.HTTPRedirect("/login")
+
+	def error_page(status, message, traceback, version):
+		return open(os.path.abspath("../html/404.html"), encoding="utf8").read()
 
 
 if __name__ == '__main__':
 	conf = {
 	   '/': {
-	       'tools.sessions.on': True
-	    }
+	       'tools.sessions.on': True,
+	       'error_page.404': AutoKrokyServer.error_page
+	    },
+	    '/public': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': os.path.abspath('../public')
+        }
 	}
 
-	cherrypy.quickstart(AutoKroky(), '/', conf)
+	cherrypy.quickstart(AutoKrokyServer(), '/', conf)
