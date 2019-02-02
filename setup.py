f7fe4e8aa16c320ae437dc044797d2447d775d93
Ljubesek <tljubesek@gmail.com>
2019-02-02 21:49:46 +0100

    Replaced cron.txt with setup.py for installing the cron job.

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ import sys
 dir_path = os.path.dirname(os.path.realpath(__file__))
 python_path = sys.executable
 
-cron_stmt = "0 12 * * 2 {} {}/lib/autokroky.py".format(python_path, dir_path)
+cron_stmt = "0 12 * * 2 {} {}/lib/autokroky.py\n".format(python_path, dir_path)
 
 with open("cron_job", "w") as file:
 	file.write(cron_stmt)
 	file.close()
 
 print("Installing cron job")
-print(cron_job)
-call(["crontab", os.abspath("cron_job")])
+print(cron_stmt)
 
-os.remove(os.abspath("cron_job"))
+call(["crontab", os.path.abspath("cron_job")])
+
+os.remove(os.path.abspath("cron_job"))
