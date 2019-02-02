a08a9ed0c33a9c4858dd1d31cc5ee9f8356e241d
Ljubesek <tljubesek@gmail.com>
2019-02-02 22:20:24 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 from subprocess import call
+import argparse
 import os
 import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--remove", action='store_true', help='Remove crontab job.')
+
+args = parser.parse_args()
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 python_path = sys.executable
 
 cron_stmt = "0 12 * * 2 {} {}/lib/autokroky.py\n".format(python_path, dir_path)
 
-with open("cron_job", "w") as file:
-	file.write(cron_stmt)
-	file.close()
-
-print("Installing cron job")
-print(cron_stmt)
-
-call(["crontab", os.path.abspath("cron_job")])
-
-os.remove(os.path.abspath("cron_job"))
+if not args.remove:
+	print("Installing cron job")
+	print(cron_stmt)
+	with open("/etc/cron.d/autokroky", "w") as file:
+		file.write(cron_stmt)
+		file.close()
+else:
+	os.remove("/etc/cron.d/autokroky")
+	print("Removed cron job")
