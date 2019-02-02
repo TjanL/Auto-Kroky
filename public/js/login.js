e5740e6492d98a38821b0dc964f1e61915ca754e
Ljubesek <tljubesek@gmail.com>
2019-02-02 20:48:50 +0100

    Removed PHP and added ajax requrests.

---
+++ public/js/login.js
@@ -0,0 +1,39 @@
+$("#login").click(function () {
+	var username = $("#username").val()
+	var password = $("#password").val()
+	$.post({
+		url: "/api/login",
+		data: JSON.stringify({"username": username, "password": password}),
+		success: function(data) {
+			if (!jQuery.isEmptyObject(data)) {
+				switch (data["status"]) {
+					case "Logged in":
+						window.location.replace("/");
+						break;
+					case "Username not registered":
+						$("#username").addClass("invalid");
+						$("[for=username]").attr("data-error", data["status"]);
+						break;
+					case "Password incorrect":
+						$("#password").addClass("invalid");
+						$("[for=password]").attr("data-error", data["status"]);
+						break;
+				}
+			} else {
+				if ($("#username").val() == "") {
+					$("#username").addClass("invalid");
+					$("[for=username]").attr("data-error", "Field empty");
+				}
+				if ($("#password").val() == "") {
+					$("#password").addClass("invalid");
+					$("[for=password]").attr("data-error", "Field empty");
+				}
+			}
+		},
+		error: function() {
+			console.dir("Error");
+		},
+		contentType: "application/json",
+		dataType: "json"
+	});
+});
