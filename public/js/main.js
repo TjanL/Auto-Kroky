2a42f7a89a4561dba04792fac1248922d856af69
Ljubesek <tljubesek@gmail.com>
2019-04-06 23:52:59 +0200

    Changed sidenav icon style to outlined and added username to title card.

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ $('.sidenav').sidenav();
 $.get({
 	url: "/api/get_user",
 	success: function(data) {
-		$("#username").text(data["username"]);
+		$(".username").text(data["username"]);
 		$('.dropdown-trigger').dropdown({
             hover: true,
             constrainWidth: false
